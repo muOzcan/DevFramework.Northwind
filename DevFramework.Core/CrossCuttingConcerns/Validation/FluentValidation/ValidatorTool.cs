67099e58b82f66e4b803b7bd35253c87cddc2b0e
Mustafa Ozcan <mustafa_ozcan_@hotmail.com>
2025-04-07 22:30:44 +0300

    General studies

---
+++ DevFramework.Core/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTool.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate.Classic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using FluentValidation;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class ValidatorTool
+    {
+        public static void FluentValidate(IValidator validator, object entity)
+        {
+            var result = validator.Validate((dynamic)entity);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
